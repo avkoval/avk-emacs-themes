647effb7c8c906834e21e99219e61e808f5ab15a
Alex Koval <alex.v.koval@labster.com>
2017-09-26 08:03:03 +0300

    Added LICENSE

--- avk-emacs-themes.el
+++ avk-emacs-themes.el
@@ -1,6 +1,19 @@
 ;;; avk-emacs-themes.el --- various themes by Alex V. Koval
 
-;; Copyright (C) 2015 Alex V. Koval
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Copyright (C) 2017 Alex V. Koval
 
 ;; Author: Alex V. Koval <alex@koval.kharkov.ua>
 ;; Maintainer: Alex V. Koval <alex@koval.kharkov.ua>
